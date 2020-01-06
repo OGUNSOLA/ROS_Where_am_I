4089f8f343ed82dce56ed042bcac8d5826011c12
OGUNSOLA <olubukola@me.com>
2020-01-06 11:09:12 -0500

    Udscity robot software engineer 'where am i' project

---
+++ where am I/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/olubukolaogunsola/workspace/catkin_ws/src
+# Build directory: /home/olubukolaogunsola/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(turtlebot_simulator/turtlebot_gazebo)
+subdirs(turtlebot_simulator/turtlebot_simulator)
+subdirs(turtlebot_simulator/turtlebot_stage)
+subdirs(turtlebot_simulator/turtlebot_stdr)
+subdirs(odom_to_trajectory)
+subdirs(robot_pose_ekf)
